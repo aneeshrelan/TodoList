e46ac31b7de514c96bc25bd588f56b69ffc772ee
Aneesh <aneesh.relan@gmail.com>
2017-01-17 17:34:49 +0530

    Design Login Page

---
+++ application/views/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login | TODO LISTer</title>
+
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+
+
+
+</head>
+<body class="container teal lighten-1">
+	<div class="row">
+		<div class="col s6 offset-s3">
+			<div class="login-form card-panel white center-align">
+				<div class="card-content center-align">
+				<h1 class="flow-text teal-text">Hi, Login to Continue</h1>
+					<img src="http://nush.sg/img/default-profile.png" width="128">
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
