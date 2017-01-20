1b94baa12bf814ae37a3242b42d4d17b73e8a978
Aneesh <aneesh.relan@gmail.com>
2017-01-20 15:41:52 +0530

    Change materialize-js, animate-css to cdn

--- application/views/login.php
+++ application/views/login.php
@@ -9,11 +9,11 @@
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
-  <script src="<?php echo asset_url(); ?>js/materialize.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
 
 
   <link rel="stylesheet" type="text/css" href="<?php echo asset_url(); ?>css/todo.css">
-  <link rel="stylesheet" type="text/css" href="<?php echo asset_url(); ?>css/animate.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css">
 
 
   <script src="<?php echo asset_url(); ?>js/todo.js"></script>
