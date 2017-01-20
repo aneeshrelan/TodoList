9bf7b101505e0eff0509db04cf29ff9dd228adb3
Aneesh <aneesh.relan@gmail.com>
2017-01-20 16:15:13 +0530

    Test register click function

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -15,10 +15,12 @@ $(document).ready(function(){
 
 	$("#registerBtn").click(function(){
 
-		$("p[class='red-text']").hide();
-		$(document).attr("title", "Register | TODOer");
-		$("#login").hide();
-		$("#register").show().addClass('animated flipInY');
+		alert('h');
+
+		// $("p[class='red-text']").hide();
+		// $(document).attr("title", "Register | TODOer");
+		// $("#login").hide();
+		// $("#register").show().addClass('animated flipInY');
 		// $("#login").addClass('animated flipOutY',$("#register").show());
 		
 	
