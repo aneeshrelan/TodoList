6c5952c68652252e1ce8b7b670f6b390bb769112
Aneesh <aneesh.relan@gmail.com>
2017-01-20 15:39:38 +0530

    Change materialize-css to cdn

--- application/views/login.php
+++ application/views/login.php
@@ -5,10 +5,10 @@
 <html>
 <head>
 <title>Login | TODOer</title>
-	<!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="<?php echo asset_url(); ?>css/materialize.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
   <script src="<?php echo asset_url(); ?>js/materialize.min.js"></script>
 
 
